8299c3d61bcfcc2d97cfd1aa666899ea4e0bed60
lijinyuan <lijinyuanc@gmail.com>
2023-11-22 22:54:31 +0800

    初步构建wifi direct的wifi发现部分

--- app/src/main/java/com/example/twinkletransfer/MainActivity.kt
+++ app/src/main/java/com/example/twinkletransfer/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.twinkletransfer
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,6 +18,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.twinkletransfer.ui.theme.TwinkleTransferTheme
 
 class MainActivity : ComponentActivity() {
+
+    //
+    private val intentFilter = IntentFilter()
+
+    private lateinit var channel: WifiP2pManager.Channel
+    private lateinit var manager: WifiP2pManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,6 +39,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+
+        manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+        channel = manager.initialize(this, mainLooper, null)
+
+
+        var broadcastReceiver = NormalReceiver()
+
     }
 }
 
@@ -42,5 +74,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     TwinkleTransferTheme {
         Greeting("Android")
+    }
+}
+
+public class NormalReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        when(intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                // Determine if Wifi P2P mode is enabled or not, alert
+                // the Activity.
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    // Wifi P2P is enabled
+                } else {
+                    // Wi-Fi P2P is not enabled
+                }
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                // The peer list has changed!  We should probably do something about
+                // that.
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                // Connection state changed!  We should probably do something about
+                // that.
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                // DeviceListFragment.updateThisDevice(
+                //     intent.getParcelableExtra(
+                //         WifiP2pManager.EXTRA_WIFI_P2P_DEVICE
+                //     ) as WifiP2pDevice
+                // )
+            }
+        }
     }
 }
