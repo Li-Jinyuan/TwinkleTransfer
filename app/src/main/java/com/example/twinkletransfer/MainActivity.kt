50c25017cc54803396df82f93745f237d97aaba0
lijinyuan <lijinyuanc@gmail.com>
2023-11-25 17:37:55 +0800

    初步构建wifi direct的wifi发现部分 经测试已经可以发现附近设备

--- app/src/main/java/com/example/twinkletransfer/MainActivity.kt
+++ app/src/main/java/com/example/twinkletransfer/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.twinkletransfer
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,15 +18,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.twinkletransfer.ui.theme.TwinkleTransferTheme
+import com.example.twinkletransfer.wifip2p.WiFiDirectBroadcastReceiver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     //
     private val intentFilter = IntentFilter()
 
-    private lateinit var channel: WifiP2pManager.Channel
-    private lateinit var manager: WifiP2pManager
+//  Indicates a change in the Wi-Fi P2P status.
+    private val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+    private var channel: WifiP2pManager.Channel? = null
+    private var receiver: BroadcastReceiver? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,25 +54,73 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-
         // Indicates a change in the Wi-Fi P2P status.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
-
         // Indicates a change in the list of available peers.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
-
         // Indicates the state of Wi-Fi P2P connectivity has changed.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
-
         // Indicates this device's details have changed.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+        channel = manager?.initialize(this, mainLooper, null)
+        receiver = WiFiDirectBroadcastReceiver(manager!!, channel!!, this)
 
-        manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
-        channel = manager.initialize(this, mainLooper, null)
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        }
+//        manager?.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+//            override fun onSuccess() {
+//                // Code for when the discovery initiation is successful goes here.
+//                // No services have actually been discovered yet, so this method
+//                // can often be left blank.  Code for peer discovery goes in the
+//                // onReceive method, detailed below.
+//                Toast.makeText(this@MainActivity, "onSuccess", Toast.LENGTH_SHORT).show()
+//            }
+//
+//            override fun onFailure(reasonCode: Int) {
+//                // Code for when the discovery initiation fails goes here.
+//                // Alert the user that something went wrong.
+//                Toast.makeText(this@MainActivity, "onFailure", Toast.LENGTH_SHORT).show()
+//
+//            }
+//        })
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            while (isActive) {
+                // 发现操作
+                manager?.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+                    override fun onSuccess() {
+                        // 处理成功情况
+                        Toast.makeText(this@MainActivity, "onSuccess", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    override fun onFailure(reasonCode: Int) {
+                        // 处理失败情况
+                        Toast.makeText(this@MainActivity, "onFailure", Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+                // 等待一段时间后再次执行发现操作（比如每隔一段时间）
+                delay(3000) // 10秒钟
+            }
+        }
 
 
-        var broadcastReceiver = NormalReceiver()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        receiver?.let { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        receiver?.let { receiver ->
+            unregisterReceiver(receiver)
+        }
     }
 }
 
@@ -77,34 +140,4 @@ fun GreetingPreview() {
     }
 }
 
-public class NormalReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        when(intent.action) {
-            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                // Determine if Wifi P2P mode is enabled or not, alert
-                // the Activity.
-                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
-                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                    // Wifi P2P is enabled
-                } else {
-                    // Wi-Fi P2P is not enabled
-                }
-            }
-            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
-                // The peer list has changed!  We should probably do something about
-                // that.
-            }
-            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
-                // Connection state changed!  We should probably do something about
-                // that.
-            }
-            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
-                // DeviceListFragment.updateThisDevice(
-                //     intent.getParcelableExtra(
-                //         WifiP2pManager.EXTRA_WIFI_P2P_DEVICE
-                //     ) as WifiP2pDevice
-                // )
-            }
-        }
-    }
-}
+
---
+++ app/src/main/java/com/example/twinkletransfer/wifip2p/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,49 @@
+package com.example.twinkletransfer.wifip2p
+
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pManager
+import android.widget.Toast
+
+/**
+ * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
+ */
+class WiFiDirectBroadcastReceiver(
+    private val manager: WifiP2pManager,
+    private val channel: WifiP2pManager.Channel,
+    private val activity: Activity
+) : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action: String = intent.action!!
+        when (action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                when (state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                        Toast.makeText(activity, "WIFI_P2P_STATE_ENABLED", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        // Wi-Fi P2P is not enabled
+                        Toast.makeText(activity, "WIFI_P2P_STATE_DISABLED", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+                Toast.makeText(activity, "WIFI_P2P_PEERS_CHANGED_ACTION", Toast.LENGTH_SHORT).show()
+            }
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                // Respond to new connection or disconnections
+                Toast.makeText(activity, "WIFI_P2P_CONNECTION_CHANGED_ACTION", Toast.LENGTH_SHORT).show()
+            }
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                // Respond to this device's wifi state changing
+                Toast.makeText(activity, "WIFI_P2P_THIS_DEVICE_CHANGED_ACTION", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
