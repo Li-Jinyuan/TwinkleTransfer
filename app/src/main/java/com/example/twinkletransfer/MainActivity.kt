fbb54f5ecb2beae902862004afafc718221841c5
lijinyuan <lijinyuanc@gmail.com>
2023-11-29 23:18:43 +0800

    增加按钮

--- app/src/main/java/com/example/twinkletransfer/MainActivity.kt
+++ app/src/main/java/com/example/twinkletransfer/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.twinkletransfer.ui.theme.TwinkleTransferTheme
 import com.example.twinkletransfer.wifip2p.WiFiDirectBroadcastReceiver
 import kotlinx.coroutines.Dispatchers
@@ -37,34 +39,23 @@ class MainActivity : ComponentActivity() {
         addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
     }
 
-//  Indicates a change in the Wi-Fi P2P status.
+    //  Indicates a change in the Wi-Fi P2P status.
     private lateinit var channel: WifiP2pManager.Channel
     private lateinit var manager: WifiP2pManager
 
     private var receiver: BroadcastReceiver? = null
 
+    // 按钮
+    private var buttonState = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TwinkleTransferTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-
-
+        setContentView(R.layout.activity_main)
 
         manager = getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
         channel = manager.initialize(this, mainLooper, null)
-
-        receiver = WiFiDirectBroadcastReceiver( this, manager, channel)
+        receiver = WiFiDirectBroadcastReceiver(this, manager, channel)
 
 
     }
@@ -73,9 +64,9 @@ class MainActivity : ComponentActivity() {
         super.onResume()
         receiver?.also { receiver ->
             registerReceiver(receiver, intentFilter)
-            discoverPeers()
         }
     }
+
     override fun onPause() {
         super.onPause()
         receiver?.also { receiver ->
@@ -83,70 +74,77 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    //init view
+    override fun onStart() {
+        super.onStart()
 
-    // 多线程启动discover peers
+        val myButton: Button = findViewById(R.id.myButton)
+        myButton.setOnClickListener {
+            // 这里是按钮点击后执行的操作
+            if (buttonState) {
+                buttonState = false
+                myButton.text = "开始"
+                Toast.makeText(this@MainActivity, "开始", Toast.LENGTH_SHORT).show()
+                discoverPeersSuspend()
+            } else {
+                buttonState = true
+                myButton.text = "停止"
+                Toast.makeText(this@MainActivity, "停止", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+    }
+
+    private fun discoverPeersSuspend() {
+        GlobalScope.launch(Dispatchers.Main) {
+            while (isActive) {
+                delay(10000)
+                Log.d("MainActivity", "discoverPeers")
+                discoverPeers()
+            }
+        }
+    }
+
     private fun discoverPeers() {
-        manager.removeGroup(channel, object : WifiP2pManager.ActionListener {
+
+        if (ActivityCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                0
+            )
+        }
+
+        val state = manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
+
             override fun onSuccess() {
-                Toast.makeText(this@MainActivity, "removeGroup onSuccess", Toast.LENGTH_SHORT).show()
+                // 处理成功情况
+                Toast.makeText(this@MainActivity, "onSuccess", Toast.LENGTH_SHORT).show()
+
             }
 
-            override fun onFailure(reason: Int) {
-                Toast.makeText(this@MainActivity, "removeGroup onFailure", Toast.LENGTH_SHORT).show()
+            override fun onFailure(reasonCode: Int) {
+                // 处理失败情况
+                if (reasonCode == WifiP2pManager.P2P_UNSUPPORTED) {
+                    Toast.makeText(this@MainActivity, "P2P_UNSUPPORTED", Toast.LENGTH_SHORT).show()
+                } else if (reasonCode == WifiP2pManager.ERROR) {
+                    Toast.makeText(this@MainActivity, "ERROR", Toast.LENGTH_SHORT).show()
+                } else if (reasonCode == WifiP2pManager.BUSY) {
+                    Toast.makeText(this@MainActivity, "BUSY", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "onFailure", Toast.LENGTH_SHORT).show()
+                }
             }
         })
-//        val job = GlobalScope.launch(Dispatchers.Default) {
-//            while (isActive) {
 
-                // check permission
-                if (ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
-                }
-
-                val state = manager.discoverPeers(channel, object : WifiP2pManager.ActionListener {
-                    override fun onSuccess() {
-                        // 处理成功情况
-                        Toast.makeText(this@MainActivity, "onSuccess", Toast.LENGTH_SHORT).show()
-
-                    }
-
-                    override fun onFailure(reasonCode: Int) {
-                        // 处理失败情况
-                        if(reasonCode == WifiP2pManager.P2P_UNSUPPORTED) {
-                            Toast.makeText(this@MainActivity, "P2P_UNSUPPORTED", Toast.LENGTH_SHORT).show()
-                        } else if (reasonCode == WifiP2pManager.ERROR) {
-                            Toast.makeText(this@MainActivity, "ERROR", Toast.LENGTH_SHORT).show()
-                        } else if (reasonCode == WifiP2pManager.BUSY) {
-                            Toast.makeText(this@MainActivity, "BUSY", Toast.LENGTH_SHORT).show()
-                        }else {
-                            Toast.makeText(this@MainActivity, "onFailure", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                })
-
-//                // 等待一段时间后再次执行发现操作（比如每隔一段时间）
-//                delay(5000) // 10秒钟
-//            }
-//        }
     }
 
 
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TwinkleTransferTheme {
-        Greeting("Android")
-    }
-}
-
 
